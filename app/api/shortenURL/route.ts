030817495153b729261a2b3f17deaf451845c46e
Zachary Memoli <zachcm@bu.edu>
2025-11-18 12:15:21 -0500

    added error handling to find post error

--- app/api/shortenURL/route.ts
+++ app/api/shortenURL/route.ts
@@ -4,24 +4,43 @@
 // but if it doesnt work, try GET
 
 import { getShortURL} from "@/app/getShortURL";
-import getCollection from "@/db";
+import { NextResponse} from "next/server"; //searched up error, says it could be due to my Response Packasge
 
 export async function POST(request: Request){
     // grabs longURL and alias
-    const{longURL, alias} = await request.json();
+    //error catching for vercel "Failed to load resource: the server responded with a status of 500 ()" error
+    try{
+        const{longURL, alias} = await request.json();
 
-    // I had to look up how to get this code, but this should
-    // return the websites URL, for concatenation
+        if(!longURL){
+            return NextResponse.json(
+                {error: "longURL not here fam"}
+            );
+        }
+        // I had to look up how to get this code, but this should
+        // return the websites URL, for concatenation
 
-    //https://medium.com/@sarfarazahmed1012/how-to-easily-get-the-base-url-in-react-or-next-js-9c49c7ccb883
-    const webURL = process.env.WEBSITE_URL!;
-    
-    //calls getShortURL function for longURL, alias
-    const returned = await getShortURL(longURL, alias); 
+        //https://medium.com/@sarfarazahmed1012/how-to-easily-get-the-base-url-in-react-or-next-js-9c49c7ccb883
+        const webURL = process.env.WEBSITE_URL!;
+        if(!webURL){
+            return NextResponse.json(
+                {error: "webURL not here fam"}
+            );
+        }
 
-    const shortendURL = `${webURL}/${returned.alias}`;
 
-    // returns response. try JSON.stringify
-    return new Response(JSON.stringify({shortendURL}))
+        //calls getShortURL function for longURL, alias
+        const returned = await getShortURL(longURL, alias); 
+
+        const shortendURL = `${webURL}/${returned.alias}`;
+
+        // returns response. try JSON.stringify
+        return NextResponse.json({shortendURL})
+    } catch (err){
+        return NextResponse.json(
+            {error: "this is the error"}
+        );
+
+    }
 
 }
--- app/components/MainDiv.tsx
+++ app/components/MainDiv.tsx
@@ -42,25 +42,40 @@ export default function MainDiv(){
         //handleClick, which sends to uRL shortener (hopefully)
     async function handleClick(){
         //remove when done
-        console.log("LongURL", longURL);
-        console.log("Alias", alias);
+        try{
+            console.log("LongURL", longURL);
+            console.log("Alias", alias);
 
 
-        //this took me way to long
-        const res = await fetch("/api/shortenURL", {
-            method: "POST",
-            body: JSON.stringify({
-                longURL, alias
-            })
-        });
+            //this took me way to long
+            const res = await fetch("/api/shortenURL", {
+                method: "POST",
+                headers: {
+                    "Content-Type":"application/json",
+                },
+                body: JSON.stringify({
+                    longURL, alias
+                })
+            });
+
+            if(!res){
+                console.error("ERROR HERE MAINDIV61");
+                return;
+            }
 
 
-        //passes data from json to variable
-        const data = await res.json();
+            //passes data from json to variable
+            const data = await res.json();
+            console.log("API response", data);
 
 
-        //makes the URL show on screen
-        setNewURL(data.shortendURL)
+            //makes the URL show on screen
+            setNewURL(data.shortendURL)
+        }
+        catch(err){
+            console.error("Network / JSON parse", err)
+        }
+        
     }
     
     return(
