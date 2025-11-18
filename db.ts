1249700e272ac981a730cdd9c1f49bc76c7d2d48
Zachary Memoli <zachcm@bu.edu>
2025-11-18 12:41:49 -0500

    checking mongodb connection

--- db.ts
+++ db.ts
@@ -14,15 +14,23 @@ let db: Db | null = null;
 
 async function connect(): Promise<Db>{
     if (!client){
-        client = new MongoClient(MONGO_URI);
-        await client.connect();
+        const tempClient = new MongoClient(MONGO_URI);
+        try {
+        await tempClient.connect();
+        client = tempClient;              // only keep it if connect() worked
+        } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+        // make sure we don't keep a bad client
+        client = null;
+        throw err;
+        }
     }
     return client.db(DB_NAME);
 }
 
 export default async function getCollection(
     collectionName: string,
-): Promise<Collection>{
+): Promise<Collection>{ 
     if (!db) {
         db = await connect();
     }
